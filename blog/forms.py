0b3192367f415db0a4a489c471c784f6b1cbcfcd
Rasul0819 <rasulabdikerimov85@gmail.com>
2024-06-22 12:00:11 +0500

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+from . import models
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields = ['username','first_name','password1','password2']
+    
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model=User
+        fields = ['username','password']
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = models.Blog
+        fields = ('__all__')
---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(blank=True)
+
+    def __str__(self) -> str:
+        return self.title
